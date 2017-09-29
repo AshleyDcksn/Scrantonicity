7c43fed375a398ff9742c894a91053633efec491
AshleyDcksn <dicksona@mail.usf.edu>
2017-09-29 18:29:06 -0400

    Send Message class for sending twilio messages

--- src/main/java/parkour/office/groovy/SendMessage.java
+++ src/main/java/parkour/office/groovy/SendMessage.java
@@ -2,20 +2,16 @@ package parkour.office.groovy;
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
+import static parkour.office.groovy.common.TwilioConst.*;
 
 public class SendMessage {
-
-
-    // Find your Account Sid and Auth Token at twilio.com/console
-    public static final String ACCOUNT_SID = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-    public static final String AUTH_TOKEN = "your_auth_token";
 
     public static void main(String[] args) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message
                 .creator(new PhoneNumber("+14159352345"), // to
-                        new PhoneNumber("+14158141829"), // from
+                        new PhoneNumber(TWILIO_PHONE_NUMBER), // from
                         "Where's Wallace?")
                 .create();
 
